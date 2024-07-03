e7b3f33a745233564928392fb834a253ffb8f348
KerollesFathy <kerolles.f@outlook.com>
2024-07-03 18:51:33 +0300

    added ETA Log doctype

---
+++ erpnext_egypt_compliance/erpnext_eta/doctype/eta_log/eta_log.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Axentor, LLC and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ETALog(Document):
+	pass
